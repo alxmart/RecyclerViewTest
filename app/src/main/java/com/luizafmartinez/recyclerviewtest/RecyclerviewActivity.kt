c5c3ccdbbc4ce81507b1bfc91e0a494dc5f37fb1
alxmart <alxmartinez@gmail.com>
2023-09-07 10:51:31 -0300

    Defined rvLista: RecyclerView

--- app/src/main/java/com/luizafmartinez/recyclerviewtest/RecyclerviewActivity.kt
+++ app/src/main/java/com/luizafmartinez/recyclerviewtest/RecyclerviewActivity.kt
@@ -2,10 +2,17 @@ package com.luizafmartinez.recyclerviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerviewActivity : AppCompatActivity() {
+
+    private lateinit var rvLista: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview)
+
+        rvLista = findViewById(R.id.rv_lista)
+
     }
 }
