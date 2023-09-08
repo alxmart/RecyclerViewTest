9a7d89a78dacbc4c865f99d697b6d53bcfa9a55b
alxmart <alxmartinez@gmail.com>
2023-09-08 15:58:02 -0300

    Tests using StaggeredGridlayoutManager.

--- app/src/main/java/com/luizafmartinez/recyclerviewtest/RecyclerviewActivity.kt
+++ app/src/main/java/com/luizafmartinez/recyclerviewtest/RecyclerviewActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class RecyclerviewActivity : AppCompatActivity() {
 
@@ -18,7 +19,7 @@ class RecyclerviewActivity : AppCompatActivity() {
 
         val lista = listOf(
             Mensagem("Jamilton","Olá, tudo bem ?","10:45"),
-            Mensagem("Ana","Te vi ontem... Lorem ipsum dolorem sit amet, dolor sit amet ipsum","00:45"),
+            Mensagem("Ana","Te vi ontem... Lorem ipsum dolorem sit amet, dolor sit amet ipsum. Lorem ipsum dolorem sit amet, dolor sit amet ipsum.","00:45"),
             Mensagem("Maria","Não acredito...","06:03"),
             Mensagem("Pedro","Futebol hoje ?","15:32"),
         )
@@ -32,6 +33,11 @@ class RecyclerviewActivity : AppCompatActivity() {
         //Configurar o layoutManager:
         //===============================================================
 
+        //StaggeredGridLayoutManager
+        rvLista.layoutManager = StaggeredGridLayoutManager(
+            2,
+            RecyclerView.VERTICAL
+        )
 
         //LinearLayoutManager ( Configurar via XML e via Código ) Define o layout que vamos utilizar
         /*rvLista.layoutManager = LinearLayoutManager(
@@ -45,6 +51,13 @@ class RecyclerviewActivity : AppCompatActivity() {
             this,
             2
         )*/
+
+        //StaggeredGridLayoutManager
+        /*rvLista.layoutManager = StaggeredGridLayoutManager(
+            2,
+            RecyclerView.VERTICAL
+        )*/
+
 
     }
 }
